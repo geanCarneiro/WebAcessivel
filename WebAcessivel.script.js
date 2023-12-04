12db40afd05f08e3d55b8147d48c6af1a1505090
geanCarneiro <geancarneiro1@gmail.com>
2023-12-04 13:30:35 -0300

    initialCommit

---
+++ WebAcessivel.script.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         WebAcessivel
+// @namespace    https://github.com/geanCarneiro/WebAcessivel
+// @version      0.1
+// @description  Script criando pra tornar a internet mais acessivel
+// @author       Gean G. Carneiro
+// @match        https://www.instagram.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=instagram.com
+// @grant        GM_xmlhttpRequest
+// @connect      github.com
+// @connect      githubusercontent.com
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    includeBaseScript();
+
+    function includeBaseScript() {
+        const baseScript = "https://raw.githubusercontent.com/geanCarneiro/Aleatorio/main/WhatsappWebWithMoreAccessibility.js";
+
+        GM_xmlhttpRequest({
+            method: "GET",
+            url: baseScript,
+            responseType: "text",
+            onload: function (res) {
+                let script = document.createElement("script");
+                script.textContent = res.responseText;
+                document.body.appendChild(script);
+            }
+        });
+    }
+})();
