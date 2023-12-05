b70e24cdf8d46901b53f0ec267ad3ac8eb52c5cf
geanCarneiro <33493032+geanCarneiro@users.noreply.github.com>
2023-12-04 23:25:56 -0300

    Update WebAcessivel.script.js

--- WebAcessivel.script.js
+++ WebAcessivel.script.js
@@ -14,20 +14,42 @@
 (function() {
     'use strict';
 
-    includeBaseScript();
+    document.addEventListener(
+        'keydown',
+        (evt) => {
+            let keyCode = evt.code;
 
-    function includeBaseScript() {
-        const baseScript = "https://raw.githubusercontent.com/geanCarneiro/WebAcessivel/main/WebAcessivel.js";
+            updatePostTitle()
 
-        GM_xmlhttpRequest({
-            method: "GET",
-            url: baseScript,
-            responseType: "text",
-            onload: function (res) {
-                let script = document.createElement("script");
-                script.textContent = res.responseText;
-                document.body.appendChild(script);
-            }
+        }, false
+    )
+
+    function notifyScreenReader(msg){
+        let ariaLive = getAriaLiveElement();
+        ariaLive.textContent = msg;
+        setTimeout(() => {
+            ariaLive.textContent = '';
+        }, 1000)
+    }
+
+    function getAriaLiveElement(){
+        let ariaLive = document.querySelector('#aria-live-elem')
+        if (!ariaLive) {
+            ariaLive = document.createElement('span');
+            ariaLive.id = 'aria-live-elem'
+            ariaLive.setAttribute('aria-live', 'assertive')
+            document.body.appendChild(ariaLive)
+        }
+        return ariaLive;
+    }
+
+    function updatePostTitle(){
+        let posts = document.querySelector('main div[style] > div > div:nth-of-type(2) > div').querySelectorAll('article[class]')
+
+        posts.forEach(post => {
+            let postOwner = post.querySelector('div > div:nth-of-type(1) span[dir] a span[dir]');
+            postOwner.setAttribute('role', 'heading')
+            postOwner.setAttribute('aria-level', '2')
         });
     }
 })();
