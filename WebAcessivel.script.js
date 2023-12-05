3a057006c0cac810883795f073cc62bf1cdc6738
geanCarneiro <geancarneiro1@gmail.com>
2023-12-05 11:09:31 -0300

    incluir e corrigir niveis de menu

--- WebAcessivel.script.js
+++ WebAcessivel.script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WebAcessivel
 // @namespace    https://github.com/geanCarneiro/WebAcessivel
-// @version      0.1
+// @version      0.2
 // @description  Script criando pra tornar a internet mais acessivel
 // @author       Gean G. Carneiro
 // @match        https://www.instagram.com/
@@ -19,10 +19,48 @@
         (evt) => {
             let keyCode = evt.code;
 
+            insertTitles()
             updatePostTitle()
 
         }, false
     )
+
+    
+
+    function insertTitles(){
+        // insert Post Title
+        let postsArea = document.querySelector('main div[style] > div > div:nth-of-type(2) > div').querySelector('article[class]')
+        postsArea = postsArea.parentNode.parentNode;
+
+        if(!postsArea.querySelector('h2')){
+            let titlePosts = document.createElement('h2')
+            titlePosts.style.width = '0'
+            titlePosts.style.height = '0'
+            titlePosts.style.overflow = 'hidden'
+            titlePosts.textContent = 'Feed'
+            postsArea.prepend(titlePosts)
+        }
+
+        // insert Stories Title
+        let storiesArea = document.querySelector('main [role="menu"] [role="presentation"]')
+        storiesArea = storiesArea.parentNode;
+
+        if(!storiesArea.querySelector('h2')){
+            let titleStories = document.createElement('h2')
+            titleStories.style.width = '0'
+            titleStories.style.height = '0'
+            titleStories.style.overflow = 'hidden'
+            titleStories.textContent = 'Stories'
+            storiesArea.prepend(titleStories)
+        }
+
+        // set Instagram as tittle
+        let instagramImg = document.querySelector('svg[aria-label="Instagram"]');
+        instagramImg = instagramImg.parentNode
+
+        instagramImg.setAttribute('role', 'heading')
+        instagramImg.setAttribute('aria-level', '1')
+    }
 
     function notifyScreenReader(msg){
         let ariaLive = getAriaLiveElement();
@@ -49,7 +87,7 @@
         posts.forEach(post => {
             let postOwner = post.querySelector('div > div:nth-of-type(1) span[dir] a span[dir]');
             postOwner.setAttribute('role', 'heading')
-            postOwner.setAttribute('aria-level', '2')
+            postOwner.setAttribute('aria-level', '3')
         });
     }
 })();
