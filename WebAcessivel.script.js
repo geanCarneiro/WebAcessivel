7ebff7b8daa2909db6f6968253f9a1249b04abcd
geanCarneiro <geancarneiro1@gmail.com>
2023-12-08 09:39:29 -0300

    correções

--- WebAcessivel.script.js
+++ WebAcessivel.script.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         WebAcessivel
+// @name         a11yInsta
 // @namespace    https://github.com/geanCarneiro/WebAcessivel
-// @version      0.3
-// @description  Script criando pra tornar a internet mais acessivel
+// @version      0.4
+// @description  Script criado pra tornar o Instagram mais acessível
 // @author       Gean G. Carneiro
 // @match        https://www.instagram.com/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=instagram.com
@@ -15,22 +15,47 @@
     'use strict';
 
     window.onload = function () {
-        document.addEventListener(
-            'keydown',
-            (evt) => {
-                let keyCode = evt.code;
+             
+        
+        let posts = document.querySelector('main div[style] > div > div:nth-of-type(2) > div').querySelectorAll('article[class]')
+        posts.forEach(post =>{
+            post.addEventListener(
+                'keydown',
+                (evt) => {
+
+                    let focusTarget;
     
-                insertTitles()
-                updatePostTitle()
-                fixDoubleSemantic()
-    
-            }, false
-        )
+                    if(evt.altKey && evt.key == 't'){
+                        focusTarget = post.querySelector('article');
+                    }
+
+                    focusTarget.focus()
+            
+                }, false
+            )
+        })
+
+        let postsArea = posts[0]
+        postsArea = postsArea.parentNode;
+
+        let observer = new MutationObserver(function(mutations) {
+            mutations.forEach(mutation => {
+                if(mutation.type == 'childList') {
+                    mutation.addedNodes.forEach(node => {
+                        updatePost(node)
+                    })
+                }
+            })
+        })
+
+        observer.observe(postsArea, {childList: true, subtree: true})
+  
+
+        // garantir a primeira execução dos metodos
         insertTitles()
-        updatePostTitle()
+        updateAllPost()
         fixDoubleSemantic()
     }
-
     
     function fixDoubleSemantic(){
         let instagramLink = document.querySelector('a[class][href="/"]');
@@ -38,6 +63,11 @@
         
         let instagramImg = instagramLink.querySelector('[role="img"]')
         instagramImg.ariaHidden = 'true'
+
+        let menu = document.querySelector('span[aria-describedby]').parentNode.parentNode.parentNode;
+        menu.querySelectorAll('svg').forEach(graph => {
+            graph.ariaHidden = 'true'
+        })
     }
     
 
@@ -77,15 +107,32 @@
         return ariaLive;
     }
 
-    function updatePostTitle(){
+    function updateAllPost() {
         let posts = document.querySelector('main div[style] > div > div:nth-of-type(2) > div').querySelectorAll('article[class]')
 
         posts.forEach(post => {
-            let postOwner = post.querySelector('div > div:nth-of-type(1) span[dir] a span[dir]');
-            postOwner = postOwner.textContent;
-
-            createTitleIn(3, 'publicação de ' + postOwner, post)
+            updatePost(post)
         });
+    }
+
+    function updatePost(post){
+        
+        let postOwner = post.querySelector('div > div:nth-of-type(1) span[dir] a span[dir]');
+        postOwner = postOwner.textContent;
+
+        createTitleIn(3, 'publicação de ' + postOwner, post)
+
+        post.tabIndex = '-1'
+
+        if(!post.querySelector('section').parentNode.querySelector('article')){
+            let textElem = post.querySelector('section').nextSibling;
+
+            let articleElem = document.createElement('article');
+            articleElem.tabIndex = '-1'
+
+            post.querySelector('section').after(articleElem)
+            articleElem.appendChild(textElem)
+        }
     }
 
     function createTitleIn(titleLevel, text, elemParent){
